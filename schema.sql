077efe8960a88fbd8a5994cd3ccbad972bf19dc8
collinsculley <collinsculley@yahoo.com>
2020-07-14 14:05:34 -0500

    updates to Schema and flow png/txt

--- schema.sql
+++ schema.sql
@@ -51,7 +51,29 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
+COPY departments FROM '/Applications/untitled folder/Data/departments.csv' WITH (FORMAT CSV, HEADER);
+
 SELECT * FROM departments;
+
+COPY employees FROM '/Users/collinsculley/DataAustin2020/AustinDataCamp/Week7/Pewlett-Hackard-Analysis/Data/employees.csv' WITH (FORMAT CSV, HEADER);
+
+SELECT * FROM employees;
+
+COPY dept_manager FROM '/Users/collinsculley/DataAustin2020/AustinDataCamp/Week7/Pewlett-Hackard-Analysis/Data/dept_manager.csv' WITH (FORMAT CSV, HEADER);
+
+SELECT * FROM dept_manager;
+
+COPY salaries FROM '/Users/collinsculley/DataAustin2020/AustinDataCamp/Week7/Pewlett-Hackard-Analysis/Data/salaries.csv' WITH (FORMAT CSV, HEADER);
+
+SELECT * FROM salaries;
+
+COPY dept_emp FROM '/Users/collinsculley/DataAustin2020/AustinDataCamp/Week7/Pewlett-Hackard-Analysis/Data/dept_emp.csv' WITH (FORMAT CSV, HEADER);
+
+SELECT * FROM dept_emp;
+
+COPY titles FROM '/Users/collinsculley/DataAustin2020/AustinDataCamp/Week7/Pewlett-Hackard-Analysis/Data/titles.csv' WITH (FORMAT CSV, HEADER);
+
+SELECT * FROM titles;
